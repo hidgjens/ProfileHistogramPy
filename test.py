9f6e14d8cd3fd3525272de6b2722ace2e9f18308
lah <loolooftw@gmail.com>
2021-05-11 10:50:49 +0100

    Made the function more interesting

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ from profilehist import PlotProfileHistogram
 
 x_data = np.linspace(-10, +10, 50)
 
-y_data = x_data * 5.0 + 3.5 + 10.0 * np.random.normal(0, 1.0, size=len(x_data))
+# generate some linear y_data with a noise term dependent on X
+y_data = (x_data * 5.0 + 3.5) + ((abs(x_data / 2.0) + 1.0) * 10.0 * np.random.normal(0, 1.0, size=len(x_data)))
 
 plt.scatter(x_data, y_data)
 plt.savefig("Original-scatter.png", bbox_inches='tight')
